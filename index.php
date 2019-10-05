e0c2d4869dd3a728f8fe7433ebc2f00a8d41fa31
insiderinside <56189841+insiderinside@users.noreply.github.com>
2019-10-05 16:15:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<?php
+session_start(); #list: key, msisdn, otp, secret_token
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Pencari Proxy © 2019</title>
+    <link rel="shortcut icon" href="https://resources.1337route.cf/favicon.ico" type="image/x-icon" />
+    <meta charset="UTF-8">
+</head>
+<?php
+    date_default_timezone_set('Asia/Jakarta');
+    
+    require_once('config.php');
+    require('class.php');
+    
+    $err    = NULL;
+    $ress   = NULL;
+    
+    if (isset($_POST) and isset($_POST['do'])){
+        
+        switch($_POST['do']){
+            
+            default: die(); exit(); break;
+
+            case "CHANGE":{
+                $key    = $_SESSION['key'];
+                $msisdn = $_SESSION['msisdn'];
+                
+                unset($_SESSION['key']);
+                unset($_SESSION['tipe']);
+                unset($_SESSION['msisdn']);
+                unset($_SESSION['otp']);
+                unset($_SESSION['secret_token']);
+                session_destroy();
+            }
+            break;
+            
+            case "LOGOUT":{
+                
+                $key            = $_SESSION['key'];
+                $msisdn         = $_SESSION['msisdn'];
+                $tipe           = $_SESSION['tipe'];
+                $otp            = $_SESSION['otp'];
+                $secret_token   = $_SESSION['secret_token'];
+                
+                
+                $tsel = new MyTsel();
+                $tsel->logout($secret_token, $tipe);
+                
+                unset($_SESSION['key']);
+                unset($_SESSION['tipe']);
+                unset($_SESSION['msisdn']);
+                unset($_SESSION['otp']);
+                unset($_SESSION['secret_token']);
+                session_destroy();
+            }
+            break;
+            
+            
+            case "GETOTP":{
+                $key    = $_POST['key'];
+                $msisdn = $_POST['msisdn'];
+                
+                
+                if ($key != privatekey){die("Error: wrong key");}
+                $tsel = new MyTsel();
+                if ($tsel->get_otp($msisdn) == "SUKSES"){
+                    
+                    session_regenerate_id();
+                    $_SESSION['key'] = $key;
+                    $_SESSION['msisdn'] = $msisdn;                    
+                    session_write_close();
+
+                }
+                else
+                {
+                    $err = "Error: msisdn salah";
+                }
+            }
+            break;
+            
+            case "LOGIN":{
+                $key    = $_SESSION['key'];
+                $msisdn = $_SESSION['msisdn'];
+                $tipe   = $_SESSION['tipe'];
+                $otp    = $_POST['otp'];
+                
+                //if ($key != privatekey){die("Error: wrong key");}
+                $tsel = new MyTsel();
+                $login = $tsel->login($msisdn, $otp, $tipe);
+                
+                
+                if (strlen($login) > 0){
+                    
+                    $secret_token               = trim(preg_replace('/\s+/', ' ', $login));
+                    $_SESSION['otp']            = $otp;
+                    $_SESSION['secret_token']   = $secret_token;
+                    $_SESSION['tipe']           = $tipe;
+                    
+                    
+                } else {
+                    //echo $login;
+                    $err = $login;
+                }
+
+                
+            }
+            break;
+            
+            case "BUY_PKG":{
+                $key            = $_SESSION['key'];
+                $msisdn         = $_SESSION['msisdn'];
+                $tipe           = $_SESSION['tipe'];
+                $secret_token   = $_SESSION['secret_token'];
+                $pkgid          = $_POST['pkgid'];
+                $transactionid  = $_POST['transactionid'];
+
+                $tsel = new MyTsel();
+                $ress = "PKGID: <b>".$pkgid."</b><br>Result: ".$tsel->buy_pkg($secret_token, $pkgid, $transactionid, $tipe);
+            }
+            break;
+            
+        }
+        
+    }
+?>
+<h3>Dor Tsel V2 Team Pencari Proxy © 2019</h3>
+<!-- ################################ 1 ################################ -->
+<?php if (!isset($_SESSION['key']) and !isset($_SESSION['msisdn']) and !isset($_SESSION['otp']) and !isset($_SESSION['secret_token']) ){ ?>
+<body>
+    <form method="POST">
+    <pre>
+MSISDN:<input type="number" name="msisdn" placeholder="628xxx"></input>
+KEY:&nbsp;&nbsp;&nbsp;<input type="text" name="key"></input>
+<input type="submit" name="do" value="GETOTP"></input>
+<?php if(!empty($err)) echo $err ?>
+    </pre>
+    </form>
+</body>
+
+<!-- ################################ 2 ################################ -->
+<?php }else if (isset($_SESSION['key']) and isset($_SESSION['msisdn']) and !isset($_SESSION['otp']) and !isset($_SESSION['secret_token']) ){ ?>
+<body>
+    <form method="POST">
+    <pre>
+<form action="">
+CHANNEL:  <input type="radio" name="tipe" value="vmp.telkomsel.com"> VMP
+  <input type="radio" name="tipe" value="vmp-preprod.telkomsel.com"> Female<br>
+</form><br>
+MSISDN: <?= $_SESSION['msisdn']; ?> <input type="submit" name="do" value="CHANGE"></input>
+ENTEROTP:<input type="number" name="otp"></input>
+<input type="submit" name="do" value="LOGIN"></input>
+<?php if(!empty($err)) echo $err ?>
+    </pre>
+    </form>
+</body>
+
+
+<!-- ################################ 3 ################################ -->
+<?php }else if (isset($_SESSION['key']) and isset($_SESSION['msisdn']) and isset($_SESSION['otp']) and isset($_SESSION['secret_token'])){ ?>
+<body>
+<form method="POST">
+<fieldset>
+Key:&nbsp;<?= $_SESSION['key']."<br>" ?>
+Msisdn:&nbsp;<?= $_SESSION['msisdn']."<br>" ?>
+OTP:&nbsp;<?= $_SESSION['otp']."<br>" ?>
+<input type="submit" name="do" value="LOGOUT"></input>
+<hr>
+<h3><u>Buy Package</u></h3>
+PKGID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="pkgid" style="width: 50%;"></input><br>
+PILIH&nbsp;PAKET: <select name="pkgid" style="width: 50%;">
+  <option value="00009382">Maxstream 1GB 2hari Rp 10</option>
+  <option value="00007333">Maxtream 30gb 30k</option>
+</select><br>
+TRANSACTIONID:<input type="text" name="transactionid" style="width: 50%;" value="A301180826192021277131740"></input><br>
+<input type="submit" name="do" value="BUY_PKG"></input><br><br>
+<?php if(!empty($ress)) echo $ress ?>
+<hr>
+</fieldset>
+</form>
+</body>
+<?php } ?>
